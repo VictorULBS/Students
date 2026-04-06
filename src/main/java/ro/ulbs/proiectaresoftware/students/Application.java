ff6e64e8d639ed63cdf877692b24455b4f6eae8d
VictorULBS <victornicolae.petrescu@ulbsibiu.ro>
2026-04-06 17:59:57 +0300

    Lab 4

--- src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Application.java
@@ -1,23 +1,95 @@
 package ro.ulbs.proiectaresoftware.students;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 
 public class Application {
+
+    public static void updateStudentGrades(String fileName) throws IOException {}
+
+    public static ArrayList<Student> readStudents(String fileName) throws IOException {
+        ArrayList<Student> students = new ArrayList<>();
+        Path path = Paths.get("students_in.txt");
+
+        Files.lines(path).forEach(line -> {
+            System.out.println(line);
+            String[] tokens = line.split(",");
+            int NumarMatricol = Integer.parseInt(tokens[0]);
+            Student s = new Student(NumarMatricol, tokens[1], tokens[2], tokens[3]);
+            s.setNota(0d);
+            students.add(s);
+        });
+
+        return students;
+    };
+
+    public static void writeStudents(ArrayList<Student> students, String fileName) throws IOException {
+
+        Path path = Paths.get("students_out.txt");
+        String text = "test text";
+        //Files.write(path, text.getBytes());
+        students.forEach(student -> {
+            String new_text = student.toString();
+            try {
+                Files.write(path, new_text.getBytes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 
     public static boolean checkStudent(List<Student> list, Student student) {
         for(Student s : list){
             if(s.getNume().equals(student.getNume())
                     && s.getPrenume().equals(student.getPrenume())
                     && s.getFormatieDeStudiu().equals(student.getFormatieDeStudiu())
-                    && s.getNumarMatricol() == student.getNumarMatricol())
+                    && s.getNumarMatricol() == student.getNumarMatricol()
+                    && s.getNota() == student.getNota())
             {
                 return true;
             }
         }
         return false;
+    }
+
+    public static ArrayList<Student> exercitiuCitireFisier(){
+        ArrayList<Student> in_out_list;
+        try{
+            in_out_list = readStudents("students.txt");
+            Comparator<Student> studentNameComparator = new Comparator<Student>() {
+                @Override
+                public int compare(Student s1, Student s2) {
+                    return s1.getNume().compareTo(s2.getNume());
+                }
+            };
+
+            Collections.sort(in_out_list, studentNameComparator);
+            //System.out.println("Rezultatul sortarii: " + in_out_list);
+            //writeStudents(in_out_list, "students_out.txt");
+            return in_out_list;
+        }
+        catch(IOException e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public static void exercitiuCautareLista(List<Student> studentList){
+        Student alis = new Student(120, "Alis", "Popa", "TI21/2");
+        alis.setNota(10d);
+
+        System.out.println("Rezultatul cautarii: " + checkStudent(studentList,
+                alis//new Student(120, "Alis", "Popa", "TI21/2")
+        ));
+        System.out.println("Rezultatul cautarii: " + checkStudent(studentList,
+                new Student(112, "Maria", "Popa", "TI21/1")
+        ));
     }
 
     public static void main(String[] args) {
@@ -28,14 +100,6 @@ public class Application {
         Student s4 = new Student(122, "Mihai", "Vecerdea", "TI22/1");
         Student s5 = new Student(122, "Eugen", "Uritescu", "TI22/2");
 
-        //STUDENT ARRAY
-        //Student[] students = {s1, s2, s3, s4, s5};
-        /*
-        for(int i=0; i<5; i++) {
-            System.out.println(students[i].toString());
-        }
-         */
-
         //STUDENT LIST
         List<Student> studentList = new ArrayList<>();
         studentList.add(s1);
@@ -43,19 +107,33 @@ public class Application {
         studentList.add(s3);
         studentList.add(s4);
         studentList.add(s5);
-        studentList.forEach(student -> System.out.println(student.toString()));
+        studentList.forEach(student -> student.setNota(0d));
+        //studentList.forEach(student -> System.out.println(student.toString()));
 
-        Student alis = new Student(120, "Alis", "Popa", "TI21/2");
+        ArrayList<Student> listFromFile = exercitiuCitireFisier();
+        HashMap<Integer, Student> studentHashMap = new HashMap<>();
+        for(Student s : listFromFile){
+            studentHashMap.put(s.getNumarMatricol(), s);
+        }
 
-        System.out.println("Rezultatul cautarii: " + checkStudent(studentList,
-                alis//new Student(120, "Alis", "Popa", "TI21/2")
-                ));
-        System.out.println("Rezultatul cautarii: " + checkStudent(studentList,
-                new Student(112, "Maria", "Popa", "TI21/1")
-                ));
+        try{
+            Path path = Paths.get("note_anon.txt");
+            Files.lines(path).forEach( line -> {
+               String[] tokens = line.split(",");
+               String token0 = tokens[0];
+               String token1 = tokens[1];
+               Integer numarMatricol = Integer.parseInt(token0);
+               Double nota = Double.parseDouble(token1);
+               studentHashMap.get(numarMatricol).setNota(nota);
+            });
 
 
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
 
+        studentHashMap.forEach( (numarMatricol, student) -> {System.out.println(studentHashMap.get(numarMatricol).toString());});
 
     }
 }
--- src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
@@ -1,10 +1,13 @@
 package ro.ulbs.proiectaresoftware.students;
 
+import java.util.Objects;
+
 public class Student {
     int numarMatricol;
     String prenume;
     String nume;
     String formatieDeStudiu;
+    Double nota;
     static boolean headerPrinted = false;
 
     public Student(int numarMatricol, String prenume, String nome, String formatieDeStudiu) {
@@ -30,14 +33,39 @@ public class Student {
         return formatieDeStudiu;
     }
 
+    public Double getNota() {
+        return nota;
+    }
+
     @Override
     public String toString() {
-        String s = String.format("%13d %10s %10s %s ", numarMatricol, prenume, nume, formatieDeStudiu);
+        String s = String.format("%13d %10s %10s %16s %10f", numarMatricol, prenume, nume, formatieDeStudiu, nota);
         String header = "";
         if(!headerPrinted){
-            header = String.format("%10s %10s %10s %10s%n", "numarMatricol", "Nume", "Prenume", "formatieDeStudiu");
+            header = String.format("%10s %10s %10s %10s %10s%n", "numarMatricol", "Nume", "Prenume", "formatieDeStudiu", "Nota");
             headerPrinted = true;
         }
         return header + s;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return numarMatricol == student.numarMatricol &&
+                nota == student.nota &&
+                Objects.equals(prenume, student.prenume) &&
+                Objects.equals(nume, student.nume) &&
+                Objects.equals(formatieDeStudiu, student.formatieDeStudiu);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numarMatricol, prenume, nume, formatieDeStudiu, nota);
     }
 }
